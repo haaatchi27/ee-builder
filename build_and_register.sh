cedeb2a461dc116014f9e9decd95de0b80a7dacd
haaatchi27 <hatchi_for_accoun2@yahoo.co.jp>
2025-11-27 19:36:15 +0900

    add alta-base repository

--- build_and_register.sh
+++ build_and_register.sh
@@ -13,24 +13,24 @@ RESISTRY="resistry" # RESISTRY : resistry or microk8s
 # cd /home/hatchi/dockers/build-awx-ee-with-cisco.ios
 cd $(dirname $0)
 
-podman build -t ${FULL_NAME}:latest -t ${FULL_NAME}:$TAG .
+podman build -t ${FULL_NAME}:latest -t ${FULL_NAME}:$TAG . || exit
 
 
 if [ "${RESISTRY}" == "resistry" ]; then
-    podman push ${FULL_NAME}:$TAG
-    podman push ${FULL_NAME}:latest
+    podman push ${FULL_NAME}:$TAG || exit
+    podman push ${FULL_NAME}:latest || exit
 fi
 
 if [ "${RESISTRY}" == "resistry" ]; then
     # import image from podman to microk8s.
-    podman save -o awx-ee-with-cisco.tar ${FULL_NAME}:latest
-    sudo microk8s ctr image import awx-ee-with-cisco.tar
+    podman save -o awx-ee-with-cisco.tar ${FULL_NAME}:latest || exit
+    sudo microk8s ctr image import awx-ee-with-cisco.tar || exit
     # sudo microk8s ctr images tag \
     #     192.168.1.8/library/awx-ee-cisco-ios:latest \
     #     myregistry.local/myproject/awx-ee-cisco-ios:v1.0
 
-    sudo microk8s ctr images ls | grep cisco
-    rm awx-ee-with-cisco.tar
+    sudo microk8s ctr images ls | grep cisco || exit
+    rm awx-ee-with-cisco.tar || exit
 fi
 exit
 
